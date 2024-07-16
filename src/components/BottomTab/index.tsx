525b9707401282f66b41e46c42c533d0906feccd
Guntur KH <guntur.kh@gmail.com>
2024-07-16 19:45:59 +0700

    feat: new screens & handle touch

---
+++ src/components/BottomTab/index.tsx
@@ -0,0 +1,35 @@
+import { binanceLogo, hamsterCoin } from '../../images'
+import Coins from '../../icons/Coins'
+import Friends from '../../icons/Friends'
+import Mine from '../../icons/Mine'
+import { useNavigate } from 'react-router-dom'
+
+function BottomTab() {
+    const navigate = useNavigate()
+    return (
+        <div className="fixed bottom-0 left-1/2 transform -translate-x-1/2 w-[calc(100%-2rem)] max-w-xl bg-[#272a2f] flex justify-around items-center z-50 rounded-3xl text-xs">
+            <div className="text-center text-[#85827d] w-1/5 bg-[#1c1f24] m-1 p-2 rounded-2xl" onClick={() => navigate('/')}>
+                <img src={binanceLogo} alt="Exchange" className="w-8 h-8 mx-auto" />
+                <p className="mt-1">Exchange</p>
+            </div>
+            <div className="text-center text-[#85827d] w-1/5" onClick={() => navigate('/mine')}>
+                <Mine className="w-8 h-8 mx-auto" />
+                <p className="mt-1">Mine</p>
+            </div>
+            <div className="text-center text-[#85827d] w-1/5">
+                <Friends className="w-8 h-8 mx-auto" />
+                <p className="mt-1">Friends</p>
+            </div>
+            <div className="text-center text-[#85827d] w-1/5">
+                <Coins className="w-8 h-8 mx-auto" />
+                <p className="mt-1">Earn</p>
+            </div>
+            <div className="text-center text-[#85827d] w-1/5">
+                <img src={hamsterCoin} alt="Airdrop" className="w-8 h-8 mx-auto" />
+                <p className="mt-1">Airdrop</p>
+            </div>
+        </div>
+    )
+}
+
+export default BottomTab
