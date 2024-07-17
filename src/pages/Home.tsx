d05fac87d4ff63f3e76bee88bafc488350bd8521
Guntur KH <guntur.kh@gmail.com>
2024-07-17 23:51:29 +0700

    feat: useTouchStartCapture

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -259,7 +259,7 @@ const Home: React.FC = () => {
             <div className="px-4 mt-4 flex justify-center">
               <div
                 className="w-60 h-60 p-4 rounded-full circle-outer"
-                onTouchStart={handleCardClick}
+                onTouchStartCapture={handleCardClick}
               >
                 <div className="w-full h-full rounded-full circle-inner">
                   <img src={mainCharacter} alt="Main Character" className="w-full h-full" />
